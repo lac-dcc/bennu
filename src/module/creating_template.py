a7f374145aa1b958d39f81ebae4c449943bbed8a
canesche <michael.canesche@gmail.com>
2023-09-04 23:14:52 -0300

    fixed some errors and FU_fixed feature

--- src/module/creating_template.py
+++ src/module/creating_template.py
@@ -25,7 +25,6 @@ class Template_autotvm():
     args = []
     search_space = [4]
     axis = None
-    order = []
 
     def __init__(self, s, t, a) -> None:
         self.sch = s
@@ -38,7 +37,6 @@ class Template_autotvm():
             self.axis.append(t)
         for t in self.sch[self.tensor].op.reduce_axis:
             self.axis.append(t)
-        #print(self.axis)
 
     def add(self, list, elements):
         for e in elements:
@@ -84,23 +82,24 @@ class Template_autotvm():
         '''
             RE_fixed: Reorder step with a list
         '''
-        assert len(list_order) == len(self.order)
+        assert len(list_order) == len(self.axis)
         p = []
         for ord in list_order:
-            p.append(self.order[ord])
+            p.append(self.axis[ord])
         self.sch[self.tensor].reorder(*p)
+        self.axis = p
 
     def RE(self, size_order):
         '''
             RE: ReorderStep
         ''' 
-        if len(self.order) == 0:
+        if len(self.axis) == 0:
             return
         name = f'RE_0'
         
         self.cfg.define_knob(name, [i for i in range(size_order)])
 
-        perms = permutation(self.order, size_order)
+        perms = permutation(self.axis, size_order)
         for i, p in enumerate(perms):
             if self.cfg[name].val == i:
                 self.sch[self.tensor].reorder(*p)
@@ -110,12 +109,12 @@ class Template_autotvm():
         '''
             SP: SplitStep
         '''
-        self.order = []
+        order = []
         for iter_id in range(len(list_iter_id)):
-            split_size = list_iter_id[iter_id]-1
+            split_size = list_iter_id[iter_id]
             if split_size == 0:
                 k = self.axis[iter_id]
-                self.add(self.order, [k])
+                self.add(order, [k])
             else:
                 for i in range(split_size):
                     name = f'SP_{iter_id}_{i}'
@@ -124,95 +123,120 @@ class Template_autotvm():
                     if i == 0:
                         x0, y0 = self.sch[self.tensor].split(self.axis[iter_id], self.cfg[name].val)
                         if i == split_size-1:
-                            self.add(self.order, [x0, y0])
+                            self.add(order, [x0, y0])
                         else:
-                            self.add(self.order, [x0])
+                            self.add(order, [x0])
                         yp = y0
                     else:
                         x, y = self.sch[self.tensor].split(yp, self.cfg[name].val)
                         if i == split_size-1:
-                            self.add(self.order, [x, y])
+                            self.add(order, [x, y])
                         else:
-                            self.add(self.order, [x])
+                            self.add(order, [x])
                         yp = y
-        self.axis = self.order # update the tensor's axis 
+        self.axis = order # update the tensor's axis 
 
-def AN(self):
-    '''
-        AN: AnnotationStep
-    '''
-    pass
+    def AN(self):
+        '''
+            AN: AnnotationStep
+        '''
+        pass
 
-def FU(self):
-    '''
-        FU: FuseStep
-    '''
-    name = f'FU_0'
-    size_fusion = len(self.axis)-1
-    self.cfg.define_knob(name, [i for i in range(size_fusion)])
+    def FU(self):
+        '''
+            FU: FuseStep
+        '''
+        name = f'FU_0'
+        size_fusion = len(self.axis)-1
+        self.cfg.define_knob(name, [i for i in range(size_fusion*size_fusion)])
 
-    for i in range(size_fusion+1):
-        if cfg[name].val == i:
-            self.sch[self.tensor].compute_at(self.axis[i], self.axis[i+1])
+        for i in range(size_fusion):
+            for j in range(size_fusion):
+                if i == j:
+                    continue
+                idx = i*size_fusion+j
+                if self.cfg[name].val == idx:
+                    fused = self.sch[self.tensor].fuse(self.axis[i], self.axis[j])
 
-def FU_fixed(self, list_fuse):
-    '''
-        FU_fixed: Fuse step with a list
-    '''
-    for i in range(len(list_fuse)-1):
-        fused = self.sch[self.tensor].fuse(self.axis[list_fuse[i]], self.axis[list_fuse[i+1]])
-        # TODO: update self.axis
+    def FU_fixed(self, list_fuse):
+        '''
+            FU_fixed: Fuse step with a list
+        '''
+        i = 0
+        p = self.axis.copy()
+        while i < len(list_fuse):
+            #print(p)
+            #print(self.axis)
+            if i == 0:
+                pfused = self.sch[self.tensor].fuse(p[list_fuse[i]], p[list_fuse[i+1]])
+                self.axis.remove(p[list_fuse[i]])
+                self.axis.remove(p[list_fuse[i+1]])
+                self.axis.insert(0, pfused)
+                i += 1
+            else:
+                fused = self.sch[self.tensor].fuse(pfused, p[list_fuse[i]])
+                self.axis.remove(pfused)
+                self.axis.remove(p[list_fuse[i]])
+                self.axis.insert(0, fused)
+                pfused = fused
+            i += 1
 
-def PR(self):
-    '''
-        PR: PragmaStep
-    '''
-    pass
+    def PR(self):
+        '''
+            PR: PragmaStep
+        '''
+        pass
 
-def FSP(self):
-    '''
-        FSP: FollowSplitStep
-    '''
-    pass
+    def PR_fixed(self, size):
+        '''
+            PR: PragmaStep
+        '''
+        print(self.axis)
 
-def FFSP(self):
-    '''
-        FFSP: FollowFusedSplitStep
-    '''
-    pass
+    def FSP(self):
+        '''
+            FSP: FollowSplitStep
+        '''
+        pass
 
-def SA(self):
-    '''
-        SA: StorageAlignStep
-    '''
-    pass
+    def FFSP(self):
+        '''
+            FFSP: FollowFusedSplitStep
+        '''
+        pass
 
-def CA(self):
-    '''
-        CA: ComputeAtStep
-    '''
-    pass
+    def SA(self):
+        '''
+            SA: StorageAlignStep
+        '''
+        pass
 
-def CI(self):
-    '''
-        CI: ComputeInlineStep
-    '''
-    pass
+    def CA(self):
+        '''
+            CA: ComputeAtStep
+        '''
+        pass
 
-def CR(self):
-    '''
-        CR: ComputeRootStep
-    '''
-    pass
+    def CI(self):
+        '''
+            CI: ComputeInlineStep
+        '''
+        pass
 
-def CHR(self):
-    '''
-        CHR: CacheReadStep
-    '''
-    pass
+    def CR(self):
+        '''
+            CR: ComputeRootStep
+        '''
+        pass
 
-def RF(self):
-    '''
-        RF: RfactorStep
-    '''
-    pass
+    def CHR(self):
+        '''
+            CHR: CacheReadStep
+        '''
+        pass
+
+    def RF(self):
+        '''
+            RF: RfactorStep
+        '''
+        pass
