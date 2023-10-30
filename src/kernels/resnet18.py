2d578fc1fc9aa3c5145cbcd52f11e78c1c0c9bbc
canesche <michael.canesche@gmail.com>
2023-10-30 13:04:41 -0300

    update resnet18

--- src/kernels/resnet18.py
+++ src/kernels/resnet18.py
@@ -2,8 +2,12 @@ import os, sys
 from tvm import relay, autotvm, auto_scheduler
 from tvm.relay import testing
 
-def resnet18(batch_size, layout="NCHW", dtype="float32"): 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 
+from src.module.template_factory import Template_factory
+
+def resnet18(batch_size, layout="NCHW", dtype="float32"):
     input_shape = (batch_size, 3, 224, 224)
     output_shape = (batch_size, 1000)
 
@@ -14,18 +18,23 @@ def resnet18(batch_size, layout="NCHW", dtype="float32"):
 
     return mod, params, input_shape, output_shape
 
+
 def resnet18_ansor(batch_size, target):
-    
     mod, params, data_shape, out_shape = resnet18(batch_size)
     tasks, task_weights = auto_scheduler.extract_tasks(mod["main"], params, target)
 
     return tasks, task_weights, mod, params, data_shape, out_shape
 
-def resnet18_autotvm(batch_size, target, cfg=None):
 
+def resnet18_autotvm(batch_size, target, cfg=None):
     mod, params, data_shape, out_shape = resnet18(batch_size)
 
     if cfg is None:
-        tasks = autotvm.task.extract_from_program(mod["main"], target=target, params=params)
-    
+        tasks = autotvm.task.extract_from_program(
+            mod["main"], target=target, params=params
+        )
+    else:
+        tasks = []
+        tasks.append(Template_factory(cfg, mod["main"], params))
+
     return tasks
