a636194d20649bbe21c0db16d83d438fc836ef5c
xintin <vermagaurav9408@gmail.com>
2023-11-05 17:37:35 -0500

    initial implementation of benchmarks

---
+++ benchmarks/depthwise.py
@@ -0,0 +1,169 @@
+import os, sys, time, argparse
+from tvm import te, topi, autotvm, auto_scheduler
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from src.template_factory import Template_factory
+from src.utils import *
+
+## ------------------ Global ---------------------
+# 128 84 83 83 5  5  2       SAME
+# N   CI H  W  KH KW Strides Padding
+input_shape = (128, 84, 83, 83)
+filter_shape = (84, 1, 5, 5)
+strides = (1, 1)
+padding = (1, 1)
+dilation = (1, 1)
+layout = "NCHW"
+dtype = "float32"
+
+
+## ----------------- Benchmark -------------------
+@auto_scheduler.register_workload
+def ansor_depthwise(input_shape, filter_shape, dtype="float32"):
+    A = te.placeholder(input_shape, name='A', dtype=dtype)
+    B = te.placeholder(filter_shape, name='B', dtype=dtype)
+    C = topi.nn.depthwise_conv2d_nhwc(A, B, stride=strides, padding=padding, dilation=dilation, out_dtype=dtype)
+
+    return [A, B, C]
+
+
+@autotvm.template("autotvm_depthwise")
+def autotvm_depthwise(input_shape, filter_shape, dtype, cfg=None):
+    A = te.placeholder(input_shape, name='A', dtype=dtype)
+    B = te.placeholder(filter_shape, name='B', dtype=dtype)
+    C = topi.nn.depthwise_conv2d_nhwc(A, B, stride=strides, padding=padding, dilation=dilation, out_dtype=dtype)
+
+    args = [A, B, C]
+    tensors = C
+
+    if cfg is not None:
+        return Template_factory(cfg, tensors, args)
+    else:
+        return te.create_schedule(C.op), args
+
+
+## ---------------------------------------------
+
+
+def generate_ansor_template(log_file, target):
+    task = tvm.auto_scheduler.SearchTask(
+        func=ansor_depthwise, args=(input_shape, filter_shape, "float32"), target=target
+    )
+
+    ## Set Parameters for Auto-Scheduler
+    trial = 1000
+    tune_option = auto_scheduler.TuningOptions(
+        num_measure_trials=trial,  # change this to 20000 to achieve the best performance
+        runner=auto_scheduler.LocalRunner(
+            number=10,
+            repeat=3,
+            timeout=100,
+            enable_cpu_cache_flush=True if target == "llvm -mcpu=a64fx" else False,
+        ),
+        measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+        verbose=0,
+    )
+
+    start = time.time()
+    # Run auto-tuning (search)
+    task.tune(tune_option)
+    end = time.time()
+
+    time_avg, best_cfg = get_best_time(log_file)
+
+    print("Time spent:", time_avg)
+    print("Config:", best_cfg)
+    print("Time spent to search:", end - start)
+
+
+def build_template(log_file, index, target):
+    config = get_template_ansor(log_file)
+
+    print(
+        "N, Droplet Time (s), Search Time (s), Count Sample, Ansor Time (s), speedup (Ansor/Droplet)"
+    )
+    for i in range(len(config)):
+        if i == index or index == -1:
+            t_ansor, cfg_ansor = config[i]
+
+            task = autotvm.task.create(
+                "autotvm_depthwise", args=(input_shape, filter_shape, "float32", cfg_ansor), target=target
+            )
+
+            measure_option = autotvm.measure_option(
+                builder="local",
+                runner=autotvm.LocalRunner(
+                    number=10,
+                    repeat=3,
+                    timeout=100,
+                    enable_cpu_cache_flush=True if target == "llvm -mcpu=a64fx" else False,
+                ),
+            )
+
+            filename = "depth.json"
+            if os.path.isfile(filename):
+                os.remove(filename)
+
+            tuner = autotvm.tuner.DropletTuner(task)
+
+            search_time = time.time()
+            tuner.tune(
+                n_trial=100,
+                measure_option=measure_option,
+                callbacks=[autotvm.callback.log_to_file(filename)],
+            )
+            search_time = time.time() - search_time
+            d_time, d_config = get_best_time(filename)
+
+            d_time = np.mean(np.array(d_time))
+            t_ansor = np.mean(t_ansor)
+
+            f = open(filename, "r")
+            count = 0
+            for l in f.readlines():
+                count += 1
+
+            print(
+                "%d, %.4f, %.2f, %d, %.4f, %.2f"
+                % (i, d_time, search_time, count, t_ansor, t_ansor / d_time)
+            )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "python depthwise.py -m ansor -a x86 -l 'results/cpu_matmul.json' -i 3"
+    )
+    parser.add_argument(
+        "-m", "--method", type=str, required=True, help="Options: ansor, droplet"
+    )
+    parser.add_argument(
+        "-a", "--arch", type=str, required=True, help="Options: x86, a64fx, cuda"
+    )
+    parser.add_argument("-l", "--logfile", type=str, required=True)
+    parser.add_argument("-i", "--index", type=int, default=-1)
+    args = parser.parse_args()
+
+    method = args.method
+    arch = args.arch
+    logfile = args.logfile
+    index = args.index
+
+    if arch == "x86":
+        target = tvm.target.Target("llvm")
+        dev = tvm.cpu()
+    elif arch == "cuda":
+        target = tvm.target.Target("cuda")
+        dev = tvm.cuda()
+    elif arch == "a64fx":
+        target = tvm.target.Target("llvm -mcpu=a64fx")
+        dev = tvm.cpu()
+    else:
+        print("Archtecture doesn't support.")
+        exit(0)
+
+    if method == "ansor":
+        generate_ansor_template(logfile, target)
+    elif method == "droplet":
+        build_template(logfile, index, target)
---
+++ benchmarks/pooling.py
@@ -0,0 +1,164 @@
+import os, sys, time, argparse
+from tvm import te, topi, autotvm, auto_scheduler
+
+from tvm.topi.nn.utils import get_pad_tuple
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from src.template_factory import Template_factory
+from src.utils import *
+
+## ------------------ Global ---------------------
+input_shape = (128, 168, 83, 83)
+dtype = "float32"
+
+#avg      128 168 83 83 1  2       VALID
+#pooltype N,  CI, H, W, K, strides padding
+
+## ----------------- Benchmark -------------------
+@auto_scheduler.register_workload
+def ansor_pool2d(input_shape, dtype="float32"):
+    A = te.placeholder(shape=input_shape, name="A", dtype=dtype)
+    B = topi.nn.pool2d(A, (1, 1), (2, 2), (1, 1), get_pad_tuple("VALID", (1, 1)), pool_type="avg")
+
+    return [A, B]
+
+
+@autotvm.template("autotvm_pool2d")
+def autotvm_pool2d(input_shape, dtype, cfg=None):
+    A = te.placeholder(shape=input_shape, name="A", dtype=dtype)
+    B = topi.nn.pool2d(A, (1, 1), (2, 2), (1, 1), get_pad_tuple("VALID", (1, 1)), pool_type="avg")
+
+    args = [A, B]
+    tensors = B
+
+    if cfg is not None:
+        return Template_factory(cfg, tensors, args)
+    else:
+        return te.create_schedule(B.op), args
+
+
+## ---------------------------------------------
+
+
+def generate_ansor_template(log_file, target):
+    task = tvm.auto_scheduler.SearchTask(
+        func=ansor_pool2d, args=(input_shape, "float32"), target=target
+    )
+
+    ## Set Parameters for Auto-Scheduler
+    trial = 1000
+    tune_option = auto_scheduler.TuningOptions(
+        num_measure_trials=trial,  # change this to 20000 to achieve the best performance
+        runner=auto_scheduler.LocalRunner(
+            number=10,
+            repeat=3,
+            timeout=100,
+            enable_cpu_cache_flush=True if target == "llvm -mcpu=a64fx" else False,
+        ),
+        measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+        verbose=0,
+    )
+
+    start = time.time()
+    # Run auto-tuning (search)
+    task.tune(tune_option)
+    end = time.time()
+
+    time_avg, best_cfg = get_best_time(log_file)
+
+    print("Time spent:", time_avg)
+    print("Config:", best_cfg)
+    print("Time spent to search:", end - start)
+
+
+def build_template(log_file, index, target):
+    config = get_template_ansor(log_file)
+
+    print(
+        "N, Droplet Time (s), Search Time (s), Count Sample, Ansor Time (s), speedup (Ansor/Droplet)"
+    )
+    for i in range(len(config)):
+        if i == index or index == -1:
+            t_ansor, cfg_ansor = config[i]
+
+            task = autotvm.task.create(
+                "autotvm_pool2d", args=(input_shape, "float32", cfg_ansor), target=target
+            )
+
+            measure_option = autotvm.measure_option(
+                builder="local",
+                runner=autotvm.LocalRunner(
+                    number=10,
+                    repeat=3,
+                    timeout=100,
+                    enable_cpu_cache_flush=True if target == "llvm -mcpu=a64fx" else False,
+                ),
+            )
+
+            filename = "pooling.json"
+            if os.path.isfile(filename):
+                os.remove(filename)
+
+            tuner = autotvm.tuner.DropletTuner(task)
+
+            search_time = time.time()
+            tuner.tune(
+                n_trial=100,
+                measure_option=measure_option,
+                callbacks=[autotvm.callback.log_to_file(filename)],
+            )
+            search_time = time.time() - search_time
+            d_time, d_config = get_best_time(filename)
+
+            d_time = np.mean(np.array(d_time))
+            t_ansor = np.mean(t_ansor)
+
+            f = open(filename, "r")
+            count = 0
+            for l in f.readlines():
+                count += 1
+
+            print(
+                "%d, %.4f, %.2f, %d, %.4f, %.2f"
+                % (i, d_time, search_time, count, t_ansor, t_ansor / d_time)
+            )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "python pooling.py -m ansor -a x86 -l 'results/cpu_matmul.json' -i 3"
+    )
+    parser.add_argument(
+        "-m", "--method", type=str, required=True, help="Options: ansor, droplet"
+    )
+    parser.add_argument(
+        "-a", "--arch", type=str, required=True, help="Options: x86, a64fx, cuda"
+    )
+    parser.add_argument("-l", "--logfile", type=str, required=True)
+    parser.add_argument("-i", "--index", type=int, default=-1)
+    args = parser.parse_args()
+
+    method = args.method
+    arch = args.arch
+    logfile = args.logfile
+    index = args.index
+
+    if arch == "x86":
+        target = tvm.target.Target("llvm")
+        dev = tvm.cpu()
+    elif arch == "cuda":
+        target = tvm.target.Target("cuda")
+        dev = tvm.cuda()
+    elif arch == "a64fx":
+        target = tvm.target.Target("llvm -mcpu=a64fx")
+        dev = tvm.cpu()
+    else:
+        print("Archtecture doesn't support.")
+        exit(0)
+
+    if method == "ansor":
+        generate_ansor_template(logfile, target)
+    elif method == "droplet":
+        build_template(logfile, index, target)
