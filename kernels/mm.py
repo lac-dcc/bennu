0e86f919a2299cf34ca704dfbc389b43b8bdfb80
canesche <michael.canesche@gmail.com>
2023-11-06 07:19:20 -0300

    adding trials option

--- kernels/mm.py
+++ kernels/mm.py
@@ -43,15 +43,14 @@ def autotvm_mm(N, K, M, dtype="float32", cfg=None):
 ## ---------------------------------------------
 
 
-def generate_ansor_template(log_file, target):
+def generate_ansor_template(log_file, target, trials):
     task = tvm.auto_scheduler.SearchTask(
         func=ansor_mm, args=(N, L, M, "float32"), target=target
     )
 
     ## Set Parameters for Auto-Scheduler
-    trial = 1000
     tune_option = auto_scheduler.TuningOptions(
-        num_measure_trials=trial,  # change this to 20000 to achieve the best performance
+        num_measure_trials=trials,  # change this to 20000 to achieve the best performance
         runner=auto_scheduler.LocalRunner(
             number=10,
             repeat=3,
@@ -74,7 +73,7 @@ def generate_ansor_template(log_file, target):
     print("Time spent to search:", end - start)
 
 
-def build_template(log_file, index, target):
+def build_template(log_file, index, target, trials):
     config = get_template_ansor(log_file)
 
     print(
@@ -109,7 +108,7 @@ def build_template(log_file, index, target):
 
             search_time = time.time()
             tuner.tune(
-                n_trial=100,
+                n_trial=trials,
                 measure_option=measure_option,
                 callbacks=[autotvm.callback.log_to_file(filename)],
             )
@@ -142,12 +141,14 @@ if __name__ == "__main__":
     )
     parser.add_argument("-l", "--logfile", type=str, required=True)
     parser.add_argument("-i", "--index", type=int, default=-1)
+    parser.add_argument("-t", "--trials", type=int, default=100)
     args = parser.parse_args()
 
     method = args.method
     arch = args.arch
     logfile = args.logfile
     index = args.index
+    trials = args.trials
 
     if arch == "x86":
         target = tvm.target.Target("llvm")
@@ -163,6 +164,6 @@ if __name__ == "__main__":
         exit(0)
 
     if method == "ansor":
-        generate_ansor_template(logfile, target)
+        generate_ansor_template(logfile, target, trials)
     elif method == "droplet":
-        build_template(logfile, index, target)
+        build_template(logfile, index, target, trials)
