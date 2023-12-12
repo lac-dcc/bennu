bb955e9b38f4ae932573bf14b4e3106f66345e10
canesche <michael.canesche@gmail.com>
2023-12-12 09:17:49 -0300

    new dependencies

--- scripts/tvm_dependencies.sh
+++ scripts/tvm_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip3 install --user numpy decorator attrs typing-extensions psutil scipy tornado 'xgboost>=1.1.0' cloudpickle
+pip3 install --user numpy decorator attrs typing-extensions psutil scipy tornado 'xgboost>=1.1.0' cloudpickle pytest synr mxnet onnx
