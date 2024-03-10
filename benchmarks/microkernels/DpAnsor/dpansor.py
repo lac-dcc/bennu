952b974f49685a9ac8df31336629a183595ccfb3
canesche <michael.canesche@gmail.com>
2024-03-10 12:19:25 -0300

    code for dpansor

--- benchmarks/microkernels/DpAnsor/dpansor.py
+++ benchmarks/microkernels/DpAnsor/dpansor.py
@@ -1,6 +1,7 @@
 import os, sys, time, argparse
 import tvm
-from tvm import auto_scheduler, te
+from tvm import auto_scheduler, te, topi
+from tvm.topi.nn.utils import get_pad_tuple
 from tvm.relay import testing
 import numpy as np
 
@@ -12,41 +13,141 @@ from src.DropletSearch import Droplet
 
 @auto_scheduler.register_workload
 def matmul_layer(batch, in_dim, out_dim):
-    data = te.placeholder((batch, in_dim), name='A', dtype="float32")
-    weight = te.placeholder((in_dim, out_dim), name='B', dtype="float32")
+    data = te.placeholder((batch, in_dim), name="A", dtype="float32")
+    weight = te.placeholder((in_dim, out_dim), name="B", dtype="float32")
     k = te.reduce_axis((0, in_dim), name="k")
-    matmul = te.compute((batch, out_dim), lambda x, y: te.sum(data[x, k] * weight[k, y], axis=k))
+    matmul = te.compute(
+        (batch, out_dim), lambda x, y: te.sum(data[x, k] * weight[k, y], axis=k)
+    )
     return [data, weight, matmul]
 
-def method_droplet(logfile, bench, top, target):
-    
+@auto_scheduler.register_workload
+def conv2d_layer(N, CI, H, W, CO, KH, KW, strides, padding):
+    data = te.placeholder((N, CI, H, W), name="input0")
+    kernel = te.placeholder((CO, CI, KH, KW), name="input1")
+    C = topi.nn.conv2d_nchw(
+        data, kernel, strides, padding, dilation=1, out_dtype="float32"
+    )
+    return [data, kernel, C]
+
+@auto_scheduler.register_workload
+def depthwise_conv2d_layer(N, CI, H, W, KH, KW, strides, padding):
+    input_shape, filter_shape= (N, CI, H, W), (CI, 1, KH, KW)
+    data = te.placeholder(input_shape, name='data', dtype="float32")
+    kernel = te.placeholder(filter_shape, name='kernel', dtype="float32")
+    depthwise_conv = topi.nn.depthwise_conv2d_nchw(data, kernel, strides, padding, dilation=1, out_dtype="float32")
+    return [data, kernel, depthwise_conv]
+
+@auto_scheduler.register_workload
+def pool_layer(pool_type, N, CI, H, W, KH, KW, strides, padding):
+    data = te.placeholder((N, CI, H, W), name="data")
+    pool = topi.nn.pool2d(
+        data,
+        (KH, KW),
+        (strides, strides),
+        (1, 1),
+        get_pad_tuple(padding, (KH, KW)),
+        pool_type=pool_type,
+    )
+    return [data, pool]
+
+@auto_scheduler.register_workload
+def reduction_layer(shape, axis, keep_dim):
+    data = te.placeholder(shape, name="data")
+    reduction = topi.sum(data, axis, keep_dim)
+    return [data, reduction]
+
+@auto_scheduler.register_workload
+def relu_layer(*shape):
+    data = te.placeholder(shape, name="data")
+    relu = topi.nn.relu(data)
+    return [data, relu]
+
+def microkernel(logfile, bench, target, top=1000):
+
     if bench == "matmul":
-        batch, in_dim, out_dim = 16, 1024, 1024
+        batch, in_dim, out_dim = 1, 1024, 1024
         matmul_layer(batch, in_dim, out_dim)
         task = auto_scheduler.SearchTask(
             func=matmul_layer, args=(batch, in_dim, out_dim), target=target
         )
-    
+    elif bench == "conv2d":
+        N, CI, H, W, CO, KH, KW, strides, padding = 128, 128, 28, 28, 128, 3, 3, 1, "SAME"
+        task = auto_scheduler.SearchTask(
+            func=conv2d_layer,
+            args=(N, CI, H, W, CO, KH, KW, strides, padding),
+            target=target,
+        )
+    elif bench == "depthwise":
+        N, CI, H, W, KH, KW, strides, padding = 128, 84, 83, 83, 5, 5, 2, "SAME"
+        task = auto_scheduler.SearchTask(
+            func=depthwise_conv2d_layer, 
+            args=(N, CI, H, W, KH, KW, strides, padding), 
+            target=target
+        )
+    elif bench == "pooling":
+        pool_type, N, CI, H, W, KH, KW, strides, padding = "avg", 128, 168, 83, 83, 1, 1, 2, "VALID"
+        task = auto_scheduler.SearchTask(
+            func=pool_layer,
+            args=(pool_type, N, CI, H, W, KH, KW, strides, padding),
+            target=target,
+        )
+    elif bench == "reduce":
+        shape, axis, keep_dim = (128, 512, 1024), 2, False
+        task = auto_scheduler.SearchTask(
+            func=layer, args=(shape, axis, keep_dim), target=target
+        )
+    elif bench == "relu":
+        shape = (4096, 4096)
+        task = auto_scheduler.SearchTask(func=relu_layer, args=shape, target=target)
+    else:
+        raise Exception(
+            f"Bench {bench} is not implemented! The benchmarks that microkernel give support are: \n -> matmul\n -> depthwise\n -> pooling\n -> reduce\n -> relu"
+        )
+
+    # clean the log
+    clean_file(logfile)
+
+    measure_ctx = auto_scheduler.LocalRPCMeasureContext(
+        number=3, repeat=1, timeout=20, min_repeat_ms=100
+    )
+
+    tune_option = auto_scheduler.TuningOptions(
+        num_measure_trials=max(10, top),  # top-k
+        runner=measure_ctx.runner,
+        measure_callbacks=[auto_scheduler.RecordToFile(logfile)],
+        verbose=0,
+    )
+
+    # Run Ansor
+    task.tune(tune_option)
+
+    # DPAnsor
     cfg = get_best_multilayers(logfile, top)
     log = f"{logfile}_droplet.log"
 
-    print("avg (ms), std (ms), time total (min)")
+    print("avg (ms), std (ms), trials, time total (min)")
     for layer, workload in enumerate(cfg):
         _, _, json_file = cfg[workload]
 
+        # DP Ansor call method
         method = Droplet(json_file, target, log)
         method.tune()
 
         droplet_avg, _ = get_best_time(log)
-        time_m, _ = get_time_total(log)
+        droplet_time, droplet_trial = get_time_total(log)
+        ansor_time, _ = get_time_total(logfile, top)
 
-        print("%.8f, %.8f, %.2f"
+        print(
+            "%.8f, %.8f, %d, %.2f"
             % (
-                np.mean(droplet_avg) * 1000, # ms
-                np.std(droplet_avg) * 1000, # ms
-                time_m / 60.0
+                np.mean(droplet_avg) * 1000,  # ms
+                np.std(droplet_avg) * 1000,  # ms
+                droplet_trial,
+                (droplet_time + ansor_time) / 60.0,
             )
         )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -57,14 +158,12 @@ if __name__ == "__main__":
     )
     parser.add_argument("-l", "--logfile", type=str, required=True)
     parser.add_argument("-b", "--benchmark", type=str, required=True)
-    parser.add_argument("-t", "--trials", type=int, default=100)
-    parser.add_argument("-k", "--top", type=int, default=1000)
+    parser.add_argument("-t", "--top", type=int, default=100)
     args = parser.parse_args()
 
     arch = args.arch
     logfile = args.logfile
     bench = args.benchmark
-    trials = args.trials
     top = args.top
 
     if arch != "cuda":
@@ -73,5 +172,5 @@ if __name__ == "__main__":
         target_name = "cuda"
         target = tvm.target.Target("cuda")
         dev = tvm.cuda()
-    
-    method_droplet(logfile, bench, top, target)
+
+    microkernel(logfile, bench, target, top)
---
+++ benchmarks/microkernels/DpAnsor/run_dpansor_topk.sh
@@ -0,0 +1,32 @@
+rm -rf log
+mkdir -p log
+mkdir -p results
+
+TOP=(
+    1
+    #10
+    #25
+    #50
+    #100
+    #200
+    #300
+    #1000
+)
+
+BENCH=(
+    #matmul
+    #conv2d
+    depthwise
+    #pooling
+    #reduce
+    #relu
+)
+
+for ((i = 0; i < ${#BENCH[@]}; i++)); do
+    echo "Executing "${BENCH[i]}"..."
+    for ((j = 0; j < ${#TOP[@]}; j++)); do
+        echo "Top-"${TOP[j]}
+        python3 dpansor.py -a cuda -l log/${BENCH[i]}.log -t ${TOP[j]} -b ${BENCH[i]} > results/${BENCH[i]}.csv
+        cat results/${BENCH[i]}.csv
+    done
+done
