fb522a21f162e3bc95b74dd657288c880e39715e
canesche <michael.canesche@gmail.com>
2024-03-10 13:40:47 -0300

    update script

--- benchmarks/microkernels/DpAnsor/run_dpansor_topk.sh
+++ benchmarks/microkernels/DpAnsor/run_dpansor_topk.sh
@@ -2,31 +2,35 @@ rm -rf log
 mkdir -p log
 mkdir -p results
 
+NAME="rtx3080"
+
 TOP=(
     1
-    #10
-    #25
-    #50
-    #100
-    #200
-    #300
-    #1000
+    10
+    25
+    50
+    100
+    200
+    300
+    1000
 )
 
 BENCH=(
-    #matmul
-    #conv2d
-    #depthwise
-    #pooling
+    matmul
+    conv2d
+    depthwise
+    pooling
     reduce
-    #relu
+    relu
 )
 
-for ((i = 0; i < ${#BENCH[@]}; i++)); do
-    echo "Executing "${BENCH[i]}"..."
-    for ((j = 0; j < ${#TOP[@]}; j++)); do
-        echo "Top-"${TOP[j]}
+for ((j = 0; j < ${#TOP[@]}; j++)); do
+    echo "Top-"${TOP[j]}
+    RESULT=results/$NAME"_"${TOP[j]}".csv"
+    echo "Top-"${TOP[j]} > $RESULT
+    for ((i = 0; i < ${#BENCH[@]}; i++)); do
+        echo "Executing "${BENCH[i]}"..."
         python3 src/dpansor.py -a cuda -l log/${BENCH[i]}.log -t ${TOP[j]} -b ${BENCH[i]} > results/${BENCH[i]}.csv
-        python3 src/print_output.py results/${BENCH[i]}.csv
+        python3 src/print_output.py results/${BENCH[i]}.csv >> $RESULT
     done
 done
