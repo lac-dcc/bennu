4dfbc441dde052bb31034ac0a478e0b06d410a58
canesche <michael.canesche@gmail.com>
2024-03-10 21:54:54 -0300

    fix error

--- benchmarks/microkernels/DpAnsor/src/dpansor.py
+++ benchmarks/microkernels/DpAnsor/src/dpansor.py
@@ -152,8 +152,8 @@ def dpansor(logfile, bench, target, top=1000):
                 np.std(droplet_avg) * 1000,  # ms
                 droplet_trial,
                 (droplet_time + ansor_time) / 60.0, # min
-                np.mean(ansor_avg_1000),
-                ansor_time_1000 * 1000,
+                np.mean(ansor_avg_1000) * 1000,
+                ansor_time_1000,
                 np.mean(ansor_avg_1000) / np.mean(droplet_avg)  
             )
         )
