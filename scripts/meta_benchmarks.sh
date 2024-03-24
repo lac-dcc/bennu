c3628e6718b478799751981321db321970048aef
canesche <michael.canesche@gmail.com>
2024-03-24 09:00:34 -0300

    update script

--- scripts/meta_benchmarks.sh
+++ scripts/meta_benchmarks.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 NAME="test" # change this
-ARCH="cuda"
-trials=1000
+ARCH="x86"
+trials=10000
 
 BENCH=(
     alexnet
+    vgg11
+    resnet18
+    resnet34
+    vgg13
+    vgg16
+    vgg19
+    shufflenet
+    squeezenet
+    resnet101
+    resnet152
+    resnet50
+    mobilenet_v2
+    mnasnet1_0
+    inception_v3
+    googlenet
     densenet121
     densenet161
     densenet169
     densenet201
-    inception_v3
-    mnasnet1_0
-    mobilenet_v2
-    resnet101
-    resnet152
-    resnet18
-    googlenet
-    resnet34
-    resnet50
-    shufflenet
-    squeezenet
-    vgg11
-    vgg13
-    vgg16
-    vgg19
 )
 
 mkdir -p results
@@ -32,7 +32,7 @@ mkdir -p results/ms
 
 for ((i = 0; i < ${#BENCH[@]}; i++)); do
     echo "BENCH: "${BENCH[i]} 
-    DIR=results/ms/meta_$ARCH"_"$NAME"_"${BENCH[i]}_1k
+    DIR=results/ms/meta_$ARCH"_"$NAME"_"${BENCH[i]}_10k
     mkdir -p $DIR
     python3 benchmarks/models_onnx.py -m meta -a $ARCH -t $trials -l $DIR -b models/${BENCH[i]}.onnx &> $DIR/output.txt
 done
