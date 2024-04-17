5888b12dc9bca701fee7650938a01d87e13c410d
canesche <michael.canesche@gmail.com>
2024-04-17 09:39:07 -0300

    create a dpmeta benchmark

---
+++ scripts/dpmeta_benchmarks.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+NAME="test" # change this
+ARCH="x86"
+trials=10000
+
+BENCH=(
+    alexnet
+    vgg11
+    resnet18
+    resnet34
+    vgg13
+    vgg16
+    vgg19
+    shufflenet
+    squeezenet
+    resnet101
+    resnet152
+    resnet50
+    mobilenet_v2
+    mnasnet1_0
+    inception_v3
+    googlenet
+    densenet121
+    densenet161
+    densenet169
+    densenet201
+)
+
+mkdir -p results
+mkdir -p results/ms
+
+for ((i = 0; i < ${#BENCH[@]}; i++)); do
+    echo "BENCH: "${BENCH[i]} 
+    DIR=results/ms/meta_$ARCH"_"$NAME"_"${BENCH[i]}_10k
+    python3 benchmarks/models_onnx.py -m dpmeta -a $ARCH -t $trials -l $DIR -b models/${BENCH[i]}.onnx 
+done
